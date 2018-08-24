66c36dc7a49f1daf743ebbb606c03fbe3a99a594
Vishal Londhe <Vishal.Londhe@meganexus.com>
2018-08-24 15:33:40 +0530

    committing

--- src/temp/Test.java
+++ src/temp/Test.java
@@ -12,11 +12,9 @@ public class Test {
 		s2.name="vishal";
 		if(s1==s2)
 
-<<<<<<< HEAD
-		System.out.println("Practice added conflicts!!!!");
-=======
+
+
 		System.out.println("MyEclipse added conflicts!!!!");
->>>>>>> 3c7c4f55026a87ea97a2e1675118476549a439d5
 
 	}
 
